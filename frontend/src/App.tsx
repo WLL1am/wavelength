5635cadc1833433f0686cd29d4ecc53643cc3ab3
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-07 13:07:03 -0800

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button variant={"outline"}>Test</Button>
+    </>
+  )
+}
+
+export default App
