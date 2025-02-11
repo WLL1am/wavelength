8de54f348f08f759f36314363e3d9d127c39217b
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-11 11:33:02 -0800

    connected to mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
+
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+
 
 dotenv.config();
 
@@ -23,4 +26,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
+    connectDB();
 });
