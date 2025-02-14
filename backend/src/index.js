caa9c5bb3ce2c74b88c996bb1aa8173582b9e420
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-14 10:13:10 -0800

    uploadToCloudinary and createSong function implementations

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,6 +37,10 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+// error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: process.env.MODE_ENV === "production" ? "Internal server error" : err.message });
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
