f677258c63595fc82e91d5499138df0e56e32f1f
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-19 00:13:03 -0800

    created topbar and SignIn button components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import HomePage from "./pages/home/HomePage";
-import { axiosInstance } from "./lib/axios";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
  
 
 function App() {
@@ -11,6 +11,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/sso-callback" 
+          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} 
+        />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
       </Routes>
